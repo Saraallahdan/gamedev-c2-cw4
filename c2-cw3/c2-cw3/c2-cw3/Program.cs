4a7af183ecd2d7cd2f27ac41c60e359241f990ad
saraallahdan-C43 <66418277+saraallahdan-C43@users.noreply.github.com>
2022-03-06 22:31:55 +0300

    c2-cw3

---
+++ c2-cw3/c2-cw3/c2-cw3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace c2_cw3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double playerSpeed = 2.5;
+
+            double SetSpeed(){
+                return playerSpeed;
+            }
+             void GetSpeed() {
+                Console.WriteLine("The current player speed is {0} ",playerSpeed);
+            }
+           GetSpeed();
+        }
+    } 
+}
